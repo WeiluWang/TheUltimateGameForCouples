bc809452b04401710cfe6e613bf504f120eff211
WeiluWang <edrj1120@gmail.com>
2023-05-25 03:09:33 -0400

    new tasks

--- src/App.js
+++ src/App.js
@@ -17,70 +17,34 @@ function App() {
 
 
     var array1 = [
-        "What was the last book I read?",
-        "What is the one thing thatwe can never agree on?",
-        "If I had to lose one of my five senses, which would it be?",
-        "Who is my favorite professional athelete?",
-        "What do I hate most about my job?",
-        "Fill in the blank: \"I can't resit a good ______.\" ",
-        "What was my favorite TV show as a kid?",
-        "What do I most often need help?",
-        "What was the most expensive item I regret buying?",
-        "How long does it take me to get ready for a nice dinner?",
-        "You have six seconds to name three animals that start with a letter of my choice.",
-        "How many times have I been pulled over by the police?",
-        "If I could eliminate one thing from your daily routine, what would it be?",
-        "If I picked our next road trip destination, where would it be?",
-        "You have eight seconds to say the first names of five of my relatives.",
-        "Name all the cities or towns that I have ever lived in.",
-        "Where would I want to travel to annually?",
-        "If I had to survive in the wilderness for a week with one other person, who would it be?",
-        "Let me put a lipstick on you.",
-        "What is my dream job?",
-        "What country do I want to visit the most?",
-        "If I had a spare room in the house, what would I use it for?",
-        "Which celebrity would I want to play me in a movie about my life?",
-        "What do I like most about my job?",
-        "Which historical figure would I like to chat with?",
-        "What is my favorite thing to do on the weekend?",
-        "Which movie or TV show do I quote the most?",
-        "If I could commit one crime and get away with it, what would it be?",
-        "What instrument do I wish I could play?",
-        "Serenade me with any song for 60 seconds.",
-        "What was my favorite subject in school?",
-        "What foreign language would I want to speak fluently?",
-        "What was my first paid job?",
-        "What am I dreading about the future?",
-        "What is something I am not afraid of that most people are?",
-        "What am I irrationally afraid of?",
-        "What was my least favorite subject in school?",
-        "What do I wish I was better at?",
-        "What is my favorite month of the year?",
-        "What I always craving?",
-        "What was the first movie I saw in theaters?",
-        "What is my most commonly used phrased?",
-        "Where is my happy place?",
-        "Which TV show have I rewatched the most?",
-        "What app do I use the most?",
-        "Who is the one person I would never want to see naked?",
-        "What do I value most in a relationship?",
-        "What was the best gift ever received from you?",
-        "What do I consider to be a major turn off?",
-        "How many bones have I broken in my life?",
-        "Let me choose a photo of you to post on your social media account.",
-        "What object is the most sentimental to me?",
-        "Where are my parents from?",
-        "What do I buy too much of?",
-        "If I could immediately master any skill inthe world, what would it be?",
-        "If I could live in a TV show, which would it be?",
-        "What was my most memorable birthday?",
-        "What is the one food I refuse to eat?",
-        "What am I most pciky about?",
-        "What am I most superstitious about?",
+    "Share an impressive dating experience with your partner",
+    "List five conditions of an ideal relationship.",
+    "Tell me what personality traits make one your ideal intimate partner and why.",
+    "Share memories about the achievements or challenges in your recent work.",
+    "Find an app related to your major and introduce it to your partner the features.",
+    "Choose a picture of your family or pets in your album and share interesting stories about it.",
+    "Draw a family portrait and show it to each other.",
+    "Tell me one thing that your family members have done for you that you appreciate.(At least two family members)",
+    "Share a song you have enjoyed recently with your partner. Introduce the background of the singer or band.",
+    "Sing a song together.",
+    "Tell me your favorite song and why it is your favorite.",
+    "Share a story about a person each other doesn’t know. It could be a friend or a person you dislike.",
+    "Choose a friend you would like to introduce him/her to your partner. Tell them the reasons and share the story about the friend and you.",
+    "Tell me how you interacted with your best friend and why your friendship lasted so long.",
+    "Talk about a movie, anime, video, or episode you like in your word to your partner.",
+    "Choose a video or movie you like and watch it with your partner",
+    "Tell me which main character in a movie you want to be and why?",
+    "Share a picture of your breakfast, lunch, and dinner with your partner.",
+    "Teach your partner to cook a dish never done before.",
+    "Tell me about the restaurant you went to which satisfied you recently. Why did you go? With who?",
+    "Share photos of the recent trip with your partner. The trip should be the one your partner didn’t take part in.",
+    " List 3 places you want to visit with your partner. Places could be countries, states, or parks. Talk about the reason and events you would like to schedule.",
+    "Take a picture on the way home and tell your partner about the story you take this picture.",
+    "Tell me about the most impressive lesson you have learned from your career in the past ten years."
     ]
 
     const drawCard = () =>{
-        setNum(randomNumberInRange(0, 59));
+        setNum(randomNumberInRange(0, 23));
         setProb(array1[num]);
         inputRef.current.value = prob;
         //console.log(prob);
@@ -129,7 +93,7 @@ function App() {
                     <div className="probDiv">
                       <textarea className="task" id="task" ref={inputRef} ></textarea>
                     </div>
-                    
+                    <div className="diarySectionTitle">Diary Feedback</div>
                     <div className="nameTitle">Name</div>
                     <div>
                       <input className="name" type="text" id="name"></input>
